e5d03e421a096b8eef7d85ea25d9e57760bdade4
Mead, Joseph <joseph.mead@accenture.com>
2023-07-25 11:25:10 -0400

    removed old code

--- spark_function.py
+++ spark_function.py
@@ -19,24 +19,6 @@ schema = StructType([StructField("name", StringType(), True),
 # Grabbing data from csv to generate a dataframe
 df = spark.read.csv('./data/demographics.csv', schema=schema)
 
-# Create a SparkSession
-spark = SparkSession.builder \
-    .appName("PySpark Functions Example") \
-    .getOrCreate()
-
-# Sample data in a list of dictionaries
-data = [
-    {"name": "john_doe", "index": "001", "gender": "male", "age": 52, "country": "USA", "state": "Alabama", "salary": 52000},
-    {"name": "jane_doe", "index": "002", "gender": "female", "age": 25, "country": "USA", "state": "California", "salary": 93250},
-    {"name": "hans_solo", "index": "003", "gender": "male", "age": 48, "country": "Corellia", "state": "NA", "salary": 1000000},
-    {"name": "darth_vader", "index": "004", "gender": "male", "age": 44, "country": "Tatooine", "state": "NA", "salary": 9999999999},
-]
-
-# Create a DataFrame from the data
-df = spark.createDataFrame(data)
-
-# Show the DataFrame
-df.show()
 
 # Function 1: Selecting columns
 selected_columns = df.select("name", "age", "salary")
